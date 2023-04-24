2c0e46871fe3029bf3f3529b669a5caa58e4b761
Copeta <aleksapecelj13@gmail.com>
2023-04-24 15:30:15 +0200

    add yupiter notebook for testing, code does not work with read data

--- combinations.py
+++ combinations.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import itertools
 
-#df=pd.read_excel('D:/Aleksa/data.xlsx')
+#df=pd.read_csv('data.csv')
 
 """
 column1 = df['Wagon_type'].tolist()
@@ -13,7 +13,6 @@ column6 = df['Cabin_Door'].tolist()
 column7 = df['Operation_Point'].tolist()
 column8 = df['Weather_Category'].tolist()
 """
-#"""
 column1 = ['MC + MC','MC + M + MC','MC + T1 + MC','MC + M1 + M1 + MC','MC + T1 + M1 + MC','MC + T1 + T1 + MC','MC + M1 + M + M1 + MC',
            'MC + M1 + T + M1 + MC','MC + T1 + M + T1 + MC','MC + M1 + M + M + M1 + MC','MC + T1 + M + M + T1 + MC','MC + T1 + M + T + T1 + MC']
 column2 = [4000,5000,6000,7000,8000]
@@ -23,10 +22,10 @@ column5 = [8,10,12]
 column6 = ['Yes','No']
 column7 = ['OP1','OP2','OP3','OP4','OP5','OP6','OP7','OP8','OP9','OP10','OP11','OP12','OP13','OP14','OP15','OP16']
 column8 = ['I','II','III']
-#"""
+
 all_columns = [column1,column2,column3,column4,column5,column6,column7,column8]
 
 combinations = list(itertools.product(*all_columns))
 df_combinations = pd.DataFrame(combinations)
 df_combinations.to_excel('combinations.xlsx', index=False)
-#print(df)
+print(df_combinations)
